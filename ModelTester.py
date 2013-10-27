5469e875c396236a87b9b5047f487b7c9dc40b6f
Carles <blauigris@gmail.com>
2013-10-27 17:48:16 +0100

    Esqueleto framework implementado

---
+++ ModelTester.py
@@ -0,0 +1,21 @@
+class ModelTester:
+	
+	def search(data, featureMethods, models):
+		accDict = {}
+		return accDict
+        
+    
+
+class FeatureSelectionMethod:
+    def __init__(self,params = None):
+        self.params = params
+    
+    def getdata(self):
+        return data
+        
+class Model:
+    def __init__(self, params = None):
+        self.params = params
+        
+    def getperformance(data):
+        return perf
