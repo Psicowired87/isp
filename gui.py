170e5076b48b541b9028ee0b6ce975b290dfd055
Carles <blauigris@gmail.com>
2013-12-21 15:28:02 +0100

    Still working

--- gui.py
+++ gui.py
@@ -1,39 +1,112 @@
 __author__ = 'carles'
 
+import modelmock as model
+import numpy as np
+
 import sys
+
 if sys.version_info[0] < 3:
     import Tkinter as tk
 else:
     import tkinter as tk
 
-class Gui:
 
+class Gui:
     def __init__(self):
-        self.root = tk.Tk()
+        self.droot = tk.Tk()
+        self.wroot = tk.Tk()
         #self.window = MainWindow(self.root)
 
-        self.dialog = OptionDialog(self.root)
+        self.dialog = OptionDialog(self.droot, self)
+        #self.dialog.hide()
+
+        self.window = MainWindow(self.wroot, self)
+        #self.window.hide()
+
+        self.model = model.Model()
 
     def run(self):
-        self.root.mainloop()
+        self.window.hide()
+        self.dialog.waitforoption()
+        self.droot.mainloop()
+        self.wroot.mainloop()
+
+
+    def chosentweet(self):
+        """
+        The user has selected to input a tweet -> show main window
+        @return:
+        """
+        self.window.show()
+
+    def chosencsv(self):
+        """
+        The user has selected to use a csv -> show file selectors
+        @return:
+        """
+        try:
+            inp = tk.tkFileDialog.askopenfile(parent=self.wroot, mode='r', title='Choose an input file')
+            out = tk.tkFileDialog.asksaveasfilename(parent=self.wroot, filetypes=("Comma separated value", "*.csv"),
+                                                    title="Save output as...")
+            if inp and out:
+                predictions = self.model.predict(list(inp))
+                inp.close()
+                np.savetxt(out, predictions, delimiter=",")
+        except (IOError, EnvironmentError) as err:
+            tk.tkMessageBox.showwarning(
+                "Error",
+                "An error has occurred {0}".format(err)
+            )
+            self.dialog.waitforoption()
+
+        tk.tkMessageBox.showinfo(
+                "Success",
+                "The operation has been processed correctly"
+            )
 
 
 
-
-
-class OptionDialog(tk.Frame):
-
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
+class HideableWindow(tk.Toplevel):
+    def __init__(self, parent, gui):
+        tk.Toplevel.__init__(self, parent)
         self.parent = parent
+        self.gui = gui
 
+    def hide(self):
+        self.parent.withdraw()
+
+    def show(self):
+        self.parent.update()
+ 
+
+class OptionDialog(HideableWindow):
+    def __init__(self, parent, gui):
+        HideableWindow.__init__(self, parent, gui)
         self.initUI()
 
     def initUI(self):
         lbl = tk.Label(self, text="Tweet weather predictor")
         lbl.grid(row=0, column=0, pady=4, padx=5)
-        tweet = tk.Button(self, text="Tweet")
+        tweet = tk.Button(self, text="Tweet", command=self.chosentweet)
         tweet.grid(row=1, column=0)
-        bck = tk.Button(self, text="CSV file")
+        bck = tk.Button(self, text="CSV file", command=self.chosencsv)
         bck.grid(row=1, column=1)
         self.pack()
+
+    def chosentweet(self):
+        self.hide()
+        self.gui.chosentweet()
+
+    def chosencsv(self):
+        self.hide()
+        self.gui.chosentweet()
+
+    def waitforoption(self):
+        self.show()
+
+
+class MainWindow(HideableWindow):
+    def __init__(self, parent, gui):
+        HideableWindow.__init__(self, parent, gui)
+
+
--- modelmock.py
+++ modelmock.py
@@ -4,5 +4,11 @@ import random
 
 
 class Model:
-    def predict(self, tweet):
-        return np.array((random.random() for i in range(24)))
+    def predict(self, tweets):
+        """
+        Gets a list with n tweets and returns a np.array of 24 columns with the masses of the labels
+        @param tweet: The n tweets as input
+        @return:np.array with dimension n x 24
+        """
+        return np.array([(random.random() for i in range(24)) for j in range(4)])
+
