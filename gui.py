485269a79922c22089df53f4010823ffac289351
Carles <blauigris@gmail.com>
2013-12-21 14:28:20 +0100

    initial dialog

--- gui.py
+++ gui.py
@@ -1,8 +1,39 @@
 __author__ = 'carles'
 
+import sys
+if sys.version_info[0] < 3:
+    import Tkinter as tk
+else:
+    import tkinter as tk
 
-class OptionDialog:
-    pass
+class Gui:
 
-class MainWindow:
-    pass
+    def __init__(self):
+        self.root = tk.Tk()
+        #self.window = MainWindow(self.root)
+
+        self.dialog = OptionDialog(self.root)
+
+    def run(self):
+        self.root.mainloop()
+
+
+
+
+
+class OptionDialog(tk.Frame):
+
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.parent = parent
+
+        self.initUI()
+
+    def initUI(self):
+        lbl = tk.Label(self, text="Tweet weather predictor")
+        lbl.grid(row=0, column=0, pady=4, padx=5)
+        tweet = tk.Button(self, text="Tweet")
+        tweet.grid(row=1, column=0)
+        bck = tk.Button(self, text="CSV file")
+        bck.grid(row=1, column=1)
+        self.pack()
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 __author__ = 'carles'
 
+import gui
 
-def
+
+
+
+
+
+def main():
+    window = gui.Gui()
+    window.run()
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
